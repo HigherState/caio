d05a2190222c2a9e940dd531da885ade43a15efc
Luis Martinez <luis3m.23@gmail.com>
2021-07-26 09:43:59 +0100

    Add const & map for InvariantAsk

--- src/main/scala/caio/mtl/InvariantAsk.scala
+++ src/main/scala/caio/mtl/InvariantAsk.scala
@@ -1,6 +1,7 @@
 package caio.mtl
 
 import cats.mtl.Ask
+import cats.Applicative
 
 trait InvariantAsk[F[_], E] extends Ask[F, E]
 
@@ -10,4 +11,19 @@ object InvariantAsk {
 
   def ask[F[_], E](implicit ask: InvariantAsk[F, E]): F[E] =
     ask.ask
+
+  def const[F[_], E](const: E)(implicit A: Applicative[F]): InvariantAsk[F, E] =
+    new InvariantAsk[F, E] {
+      val applicative: Applicative[F] = A
+
+      def ask[E2 >: E]: F[E2] = A.pure(const)
+    }
+
+  def map[F[_], E, E1](f: E => E1)(implicit IA: InvariantAsk[F, E]): InvariantAsk[F, E1] =
+    new InvariantAsk[F, E1] {
+      val applicative: Applicative[F] = IA.applicative
+
+      def ask[E2 >: E1]: F[E2] =
+        IA.applicative.map(IA.ask)(f)
+    }
 }
